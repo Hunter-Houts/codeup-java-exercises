015be93db3f799eb72903243110e1bdca75d28c4
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-09-17 12:25:04 -0500

    Finished Syntax, Data Types, and Variables exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
     public static void main(String[] args){
+        double myFavoriteNumber = 3.14;
+        System.out.println(myFavoriteNumber);
+        String myString = "This is a String";
+        System.out.println(myString);
         System.out.println("Hello World");
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//       int test = 2147483648;
     }
 }
