7489f4c2380f41f26f3c7c5b90361c7c83af79aa
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-09-18 11:35:21 -0500

    in class Pushups

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
-public class sandbox {
+public class Sandbox {
+    public static void main(String[] args){
+        int wholeNumber = 1;
+        short shortNumber = 12333;
+        byte bite = 0b10;
+        long longNumber = 1123123123;
+        float floatNumber = 1.1f;
+        double doubleNumber = 1.1234;
+        char charVar = 'a';
+        boolean bool = true;
+        String stringVar = "this is a string";
+        System.out.printf("%d %d %d %d %f %f %c %b %s",wholeNumber,shortNumber,bite,longNumber,floatNumber,doubleNumber,charVar,bool,stringVar);
+    }
 }
