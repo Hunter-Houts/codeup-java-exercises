b0d2cca3ac9e9c215d3509f54dc964638a2e28c4
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-09-18 13:58:13 -0500

    String Exercise Complete

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String message = "We don't need no education\nWe don't need no thought control\nCheck \"This\" out!, \"s inside of \"s!\nIn windows, the main drive is usually C:\\\nI can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+    }
+
+}
