7a803178ce5a4c13a99dfd6350eac593743f7def
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-09-19 11:08:10 -0500

    do while loop

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -10,5 +10,14 @@ public class Sandbox {
         boolean bool = true;
         String stringVar = "this is a string";
         System.out.printf("%d %d %d %d %f %f %c %b %s",wholeNumber,shortNumber,bite,longNumber,floatNumber,doubleNumber,charVar,bool,stringVar);
+        long num = 2;
+        do {
+            if(num == 2){
+                System.out.printf("%d%n",num);
+            }
+            System.out.printf("%d%n",((long) Math.pow(num,2)));
+            num = (long) Math.pow(num,2);
+        }while(num < 1000000);
+        }
     }
-}
+
