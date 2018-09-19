64088a6f1586241f1c4abf885fb06e12ac95f494
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-09-19 16:49:18 -0500

    Went down a horrible rabbit hole

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Sandbox {
+    public static Integer realLength(int a[]){
+        int length = 0;
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != 0){
+                length++;
+            }
+        }
+
+        return length;
+    }
     public static void main(String[] args){
         int wholeNumber = 1;
         short shortNumber = 12333;
@@ -9,7 +23,7 @@ public class Sandbox {
         char charVar = 'a';
         boolean bool = true;
         String stringVar = "this is a string";
-        System.out.printf("%d %d %d %d %f %f %c %b %s",wholeNumber,shortNumber,bite,longNumber,floatNumber,doubleNumber,charVar,bool,stringVar);
+        System.out.printf("%d %d %d %d %f %f %c %b %s%n",wholeNumber,shortNumber,bite,longNumber,floatNumber,doubleNumber,charVar,bool,stringVar);
         long num = 2;
         do {
             if(num == 2){
@@ -18,6 +32,15 @@ public class Sandbox {
             System.out.printf("%d%n",((long) Math.pow(num,2)));
             num = (long) Math.pow(num,2);
         }while(num < 1000000);
+        int[] arr = new int[15];
+        System.out.println(Arrays.toString(arr));
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[3] = 3;
+        int arrLength = arr.length;
+        System.out.println(arrLength);
+        System.out.println(realLength(arr));
         }
+
     }
 
