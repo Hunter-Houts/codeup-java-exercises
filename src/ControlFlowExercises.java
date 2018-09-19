c4dbac719de403bde12ff7d4a1046abcf1a63d0f
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-09-19 12:18:30 -0500

    Forgot to ask to continue, added that part

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class ControlFlowExercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        int i = 5;
 //        while(i <= 15){
 //            System.out.println(i);
@@ -12,65 +12,73 @@ public class ControlFlowExercises {
 //            j -= 5;
 //
 //        } while(j >= -10);
-        for(int i = 5; i <= 15; i++){
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
         }
-        for(int j = 100; j >= -10; j-=5){
+        for (int j = 100; j >= -10; j -= 5) {
             System.out.printf(j + "%n%n");
         }
 
-        for(int k = 1; k <= 100; k++){
-                if(k % 15 == 0){
-                    System.out.println("FizzBuzz");
-                } else if (k % 3 == 0){
-                    System.out.println("Fizz");
-                } else if (k % 5 == 0){
-                    System.out.println("Buzz");
-                } else {
-                    System.out.println(k);
-                }
-            }
-            Scanner s = new Scanner(System.in);
-            System.out.print("What number would you like to go up to?");
-            int userNumber = s.nextInt();
-            System.out.println("Here is your table!");
-            System.out.println();
-            System.out.println("Number | Squared | Cubed");
-            System.out.println("------ | ------- | -----");
-            if (userNumber < 1291) {
-                for (int i = 1; i <= userNumber; i++) {
-                    if ((int) Math.pow(i, 2) < 10) {
-                        System.out.printf("%d      | %s       | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-                    } else if ((int) Math.pow(i, 2) < 100) {
-                        System.out.printf("%d      | %s      | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-                    } else if ((int) Math.pow(i, 2) < 1000) {
-                        System.out.printf("%d     | %s     | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-                    } else if ((int) Math.pow(i, 2) < 10000) {
-                        System.out.printf("%d     | %s    | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-                    } else if ((int) Math.pow(i, 2) < 100000) {
-                        System.out.printf("%d    | %s   | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-                    } else if ((int) Math.pow(i, 2) < 1000000) {
-                        System.out.printf("%d    | %s  | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-                    } else {
-                        System.out.printf("%d   | %s | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-                    }
-                }
+        for (int k = 1; k <= 100; k++) {
+            if (k % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (k % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (k % 5 == 0) {
+                System.out.println("Buzz");
             } else {
-                System.out.println("Number too big");
+                System.out.println(k);
             }
+        }
+        Scanner s = new Scanner(System.in);
+        System.out.print("What number would you like to go up to?");
+        int userNumber = s.nextInt();
+        System.out.println("Here is your table!");
+        System.out.println();
+        System.out.println("Number | Squared | Cubed");
+        System.out.println("------ | ------- | -----");
+        if (userNumber < 1291) {
+            for (int i = 1; i <= userNumber; i++) {
+                if ((int) Math.pow(i, 2) < 10) {
+                    System.out.printf("%d      | %s       | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                } else if ((int) Math.pow(i, 2) < 100) {
+                    System.out.printf("%d      | %s      | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                } else if ((int) Math.pow(i, 2) < 1000) {
+                    System.out.printf("%d     | %s     | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                } else if ((int) Math.pow(i, 2) < 10000) {
+                    System.out.printf("%d     | %s    | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                } else if ((int) Math.pow(i, 2) < 100000) {
+                    System.out.printf("%d    | %s   | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                } else if ((int) Math.pow(i, 2) < 1000000) {
+                    System.out.printf("%d    | %s  | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                } else {
+                    System.out.printf("%d   | %s | %s    %n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+                }
+            }
+        } else {
+            System.out.println("Number too big");
+        }
+        boolean confirm = true;
+        while (confirm) {
             System.out.print("Enter your grade (0-100): ");
             int userGrade = s.nextInt();
-            if(userGrade < 88 && userGrade >= 80){
+            if (userGrade < 88 && userGrade >= 80) {
                 System.out.println("Your grade is a B");
-            } else if(userGrade < 80 && userGrade >= 67){
+            } else if (userGrade < 80 && userGrade >= 67) {
                 System.out.println("Your grade is a C");
-            } else if (userGrade < 67 && userGrade >= 60){
+            } else if (userGrade < 67 && userGrade >= 60) {
                 System.out.println("Your grade is a D");
-            } else if (userGrade < 60){
+            } else if (userGrade < 60) {
                 System.out.println("Your grade is a F");
             } else {
                 System.out.println("Your grade is an A");
             }
+            System.out.println("Do you want to continue?: [y/n]");
+            String answer = s.next();
+            if(!answer.toLowerCase().startsWith("y")){
+                confirm = false;
+            }
         }
+    }
     }
 
