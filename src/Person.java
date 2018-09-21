f91b7b1fdc44943002242a60454e39e3e45bea7e
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-09-21 11:14:00 -0500

    Finished person exercise

---
+++ src/Person.java
@@ -0,0 +1,37 @@
+public class Person {
+    private String name;
+     Person(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void sayHello(){
+        System.out.println("Hello " + getName());
+    }
+
+    public static void main(String[] args) {
+        Person thePerson = new Person("Hunter");
+        System.out.println(thePerson.getName());
+        thePerson.setName("Blue");
+        System.out.println(thePerson.getName());
+        thePerson.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+    }
+}
